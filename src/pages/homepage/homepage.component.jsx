5a4b1cd1a74f495aac5d1d181a64d7d9b39f9166
Shivam Chopra <shivam1112010@gmail.com>
2021-05-26 01:11:10 +0530

    added github link

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,6 +5,8 @@ import OrderSummary from '../../components/order-summary/order-summary.component
 const Homepage = () =>{
     return (
         <div className='w-full h-full bg-gray-100 flex flex-col justify-center items-center' >
+    <a href='https://github.com/shivamChopraCodes/pepe-jeans-tracking' className='absolute top-0 right-0 mr-2 underline p-1 text-gray-50 bg-black rounded-md border-gray-400' >Github Link</a>
+        
         <OrderSummary />
         <DisplayOtherItems />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
-    enabled: true,
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html']
+    // enabled: true,
+    // content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html']
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
