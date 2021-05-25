9552a898721d072cf08cf936c6078c05a4257bed
Shivam Chopra <shivam1112010@gmail.com>
2021-05-26 01:02:03 +0530

    added purged css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
